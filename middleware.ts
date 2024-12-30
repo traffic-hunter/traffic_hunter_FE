f45be49130b12e3939be506a3044df5a9da94f64
박현우 <gusdn3477@naver.com>
2024-12-30 21:47:06 +0900

    feat: middleware 사용하여 로그인 된 경우 로그인 페이지 진입 못 하도록 구현

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,15 @@
-export { auth as middleware } from "@/auth"
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req : NextRequest) {
+  const { pathname } = req.nextUrl;
+
+  // 쿠키에서 세션 토큰 확인
+  const sessionToken = req.cookies.get("authjs.session-token");
+
+  // 로그인된 사용자가 로그인 페이지에 접근 시 리다이렉트
+  if (pathname === "/api/auth/signin" && sessionToken) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return NextResponse.next();
+}
