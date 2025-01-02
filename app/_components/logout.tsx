2f0748ce063d1897220e8cac8c37892448dd4c5d
박현우 <gusdn3477@naver.com>
2025-01-02 23:56:46 +0900

    fix: logout 컴포넌트 use client 쓰는 방식으로 원상복구(서버 액션하면 에러 발생해서 원인 파악 필요)

--- app/_components/logout.tsx
+++ app/_components/logout.tsx
@@ -1,12 +1,12 @@
+'use client'
+
 import { signOut } from "next-auth/react"
 
-export default function Logout(){
+export default function Logout() {
 
-    return (
-        <form action={async () => {
-            "use server"
-            await signOut();
-        }}>
-            <button type="submit">로그아웃</button>
-        </form>)
+    const handleLogout = async () => {
+        await signOut();
+    }
+
+    return (<button onClick={handleLogout}>로그아웃</button>)
 }
